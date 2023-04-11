319b008a96bffc0b2beeda05e11b1b7aff68db48
arun <arun.kumar2@tothenew.com>
2023-04-11 15:57:45 +0530

    add

---
+++ src/main/java/selenium123/Calculator.java
@@ -0,0 +1,22 @@
+package selenium123;
+
+public class Calculator {
+	
+	public int add(int a, int b) {
+		return a+b;
+	}
+	
+	public int sub(int a, int b) {
+		return a-b;
+	}
+	
+	public int mul(int a, int b) {
+		return a*b;
+	}
+	
+	public int div(int a, int b) {
+		return a/b;
+	}
+}
+
+
